8c702961b41f85dee897bc154fc6b9cee399f2b7
John Bruckler <john.bruckler@dtmidstream.com>
2023-10-15 00:29:31 -0400

    merge from work branch

---
+++ src/PSIDM.Universal/Functions/Private/Export-PSIDMConfig.ps1
@@ -0,0 +1,81 @@
+function Export-PSIDMConfig {
+    <#
+    .SYNOPSIS
+        Exports the PSIDM configuration to a JSON file.
+
+    .DESCRIPTION
+        The Export-PSIDMConfig function exports the PSIDM configuration stored
+        in a PSCustomObject to a JSON file.  If the UpdateSession switch is
+        provided, the function will also update the current session's configuration,
+        defined in $Script:PSIDM and accessible through Get-PSIDMConfig.
+
+    .PARAMETER InputObject
+        The configuration object to be exported. Defaults to the script-scoped
+        variable $Script:PSIDM.
+
+    .PARAMETER UpdateSession
+        If this switch is provided, the function will update the current session's
+        configuration using Import-PSIDMConfig.
+
+    .EXAMPLE
+        Export-PSIDMConfig -UpdateSession
+
+        This example exports the current $Script:PSIDM configuration to the
+        default path and updates the session.
+
+    .NOTES
+        If the specified path does not exist, the function will attempt to create
+        it.
+
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [Alias('Config')]
+        [System.Collections.Hashtable] $InputObject = $Script:PSIDM,
+
+        [switch] $UpdateSession
+    )
+
+    begin {
+        $configFileMap = Get-ConfigFileMap
+    }
+    process {
+        Write-Debug "InputObject: $($InputObject | ConvertTo-Json -Depth 10)"
+        if (-not $InputObject) {
+            Write-Debug 'No InputObject was passed, and $Script:PSIDM is null.'
+            throw [System.ArgumentNullException] 'No configuration available. Please provide a configuration object or ensure the script has a valid configuration.'
+        }
+
+        $configFileMap.GetEnumerator() | ForEach-Object {
+            $configName = $_.Key
+            $configPath = $_.Value
+
+            Write-Debug "Config name: $configName"
+            Write-Debug "Config path: $configPath"
+
+            if ($null -eq $InputObject.$configName) {
+                Write-Debug "Configuration '$configName' is null. Skipping."
+                continue
+            }
+            else {
+                Write-Debug "Exporting configuration '$configName' to '$configPath'."
+                Write-Verbose "Exporting configuration '$configName' to '$configPath'."
+                $InputObject.$configName | ConvertTo-Json -Depth 10 | Out-File -FilePath $configPath -Encoding UTF8 -Force
+
+                Write-Debug "Checking if we need to update the session..."
+                if ($UpdateSession) {
+                    if (-not(Get-Variable -Name PSIDM -ErrorAction SilentlyContinue -Scope Script)) {
+                        Write-Debug "Session configuration does not exist. Creating..."
+                        $Script:PSIDM = @{
+                            'Module'    = @{ }
+                            'Jobs'      = @{ }
+                        }
+                    }
+                    Write-Verbose "Updating session configuration with '$configName'..."
+                    $Script:PSIDM[$configName] = $InputObject.$configName
+                }
+            }
+        }
+    }
+}
---
+++ src/PSIDM.Universal/Functions/Private/Format-PrettyPrintHashtable.ps1
@@ -0,0 +1,29 @@
+function Format-PrettyPrintHashtable {
+    param (
+        [Parameter( Mandatory = $true,
+                    valueFromPipeline = $true,
+                    Position = 0 )]
+        [Hashtable]$Hashtable,
+        [int]$Indent = 0
+    )
+
+    $maxKeyLength = ($Hashtable.Keys | Measure-Object -Maximum -Property Length).Maximum
+
+    foreach ($key in $Hashtable.Keys) {
+        $value = $Hashtable[$key]
+        $indentation = " " * $Indent
+        $keyPadding = " " * ($maxKeyLength - $key.Length)
+
+        if ($value -is [Hashtable]) {
+            Write-Host "${indentation}${key}${keyPadding} :"
+            Format-PrettyPrintHashtable -Hashtable $value -Indent ($Indent + 4)
+        } elseif ($value -is [Array]) {
+            Write-Host "${indentation}${key}${keyPadding} :"
+            foreach ($item in $value) {
+                Write-Host "${indentation}    ${item}"
+            }
+        } else {
+            Write-Host "${indentation}${key}${keyPadding} : $value"
+        }
+    }
+}
